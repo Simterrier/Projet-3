02bfcb2472821a5757191c9143a60a3aca45c009
Terrier Simon <simonterrier110994@gmail.com>
2025-07-22 17:05:38 +0200

    ajout bouton filter js

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -10,6 +10,8 @@ fetch(works)
   })
   .then((projects) => {
     projects.forEach((project) => {
+      //   console.log(project);
+
       const figure = document.createElement("figure");
 
       const img = document.createElement("img");
@@ -28,3 +30,62 @@ fetch(works)
   .catch((error) => {
     console.error("Erreur lors du chargement des projets :", error);
   });
+
+// BOUTONS
+
+const btnfilters = document.getElementById("filters");
+
+fetch("http://localhost:5678/api/categories")
+  .then((res) => res.json())
+  .then((categories) => {
+    const allButton = document.createElement("button");
+    allButton.textContent = "Tous";
+    allButton.dataset.categoryId = "all";
+    allButton.classList.add("filter-btn");
+    btnfilters.appendChild(allButton);
+
+    categories.forEach((category) => {
+      const button = document.createElement("button");
+      button.textContent = category.name;
+      button.dataset.categoryId = category.id;
+      button.classList.add("filter-btn");
+      btnfilters.appendChild(button);
+    });
+
+    const buttons = document.querySelectorAll(".filter-btn");
+    buttons.forEach((btn) => {
+      btn.addEventListener("click", () => {
+        const selectedId = btn.dataset.categoryId;
+        filterProjects(selectedId);
+      });
+    });
+  });
+
+function filterProjects(categoryId) {
+  fetch("http://localhost:5678/api/works")
+    .then((res) => res.json())
+    .then((projects) => {
+      gallery.innerHTML = "";
+
+      const filteredProjects =
+        categoryId === "all"
+          ? projects
+          : projects.filter(
+              (project) => project.category.id === parseInt(categoryId)
+            );
+
+      filteredProjects.forEach((projects) => {
+        const figure = document.createElement("figure");
+        const img = document.createElement("img");
+        img.src = projects.imageUrl;
+        img.alt = projects.title;
+
+        const caption = document.createElement("figcaption");
+        caption.textContent = projects.title;
+
+        figure.appendChild(img);
+        figure.appendChild(caption);
+        gallery.appendChild(figure);
+      });
+    });
+}
