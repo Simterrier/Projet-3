386ff0e2f57f314d89a7a1801c7afc5f980a9ac7
Terrier Simon <simonterrier110994@gmail.com>
2025-07-22 17:29:51 +0200

    stylisation des boutons filtre css + js

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -55,6 +55,8 @@ fetch("http://localhost:5678/api/categories")
     const buttons = document.querySelectorAll(".filter-btn");
     buttons.forEach((btn) => {
       btn.addEventListener("click", () => {
+        buttons.forEach((b) => b.classList.remove("active"));
+        btn.classList.add("active");
         const selectedId = btn.dataset.categoryId;
         filterProjects(selectedId);
       });
